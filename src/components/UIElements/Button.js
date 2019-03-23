8a5a8533a69166ab618d2330edbfabd53f3dbf1e
Rodolfo Perez <rodolfobeste@gmail.com>
2019-03-23 17:46:01 +0200

    Added type property to button component

--- src/components/UIElements/Button.js
+++ src/components/UIElements/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = props => {
     return(
-        <button className={ props.stylingClass }>
+        <button className={ props.stylingClass } type={ props.type }>
             { props.text }
         </button>
     )
