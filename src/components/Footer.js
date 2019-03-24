8f567138d20031f36b071a3f81e5586fe18e215b
Rodolfo Perez <rodolfobeste@gmail.com>
2019-03-25 01:28:25 +0200

    Added extra buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,8 @@ import Button from "../components/UIElements/Button";
 const Footer = () => {
     return(
         <footer>
+            <Button text="A" stylingClass="footer_button"/>
+            <Button text="B" stylingClass="footer_button"/>
             <Button text="Import" stylingClass="footer_button"/>
             <Button text="Export" stylingClass="footer_button"/>
         </footer>
